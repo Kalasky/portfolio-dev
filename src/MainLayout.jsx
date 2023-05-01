1d16166b817b08bae3c3fed58ae30930da72e17d
wiu <kalaskyr@gmail.com>
2023-05-01 00:24:46 -0400

    vercel configurations

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const MainLayout = ({ children }) => {
+  // gets the current location object and assigns it to the location variable
   const location = useLocation()
 
   useEffect(() => {
     window.scrollTo(0, 0)
-  }, [location])
+  }, [location]) // the effect will run when the location changes
 
+  // the children prop will be the components you want to wrap with MainLayout
   return <>{children}</>
 }
 
